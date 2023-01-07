755785ea10eed5f079696f218def38bd9def75ab
KorvusKroax <korvuskroax@gmail.com>
2023-01-07 14:27:25 +0100

    wip

--- contactbar.php
+++ contactbar.php
@@ -1,5 +1,6 @@
 <div class="contactbar">
 
+    <!-- address -->
     <a class="contactbar-item" href="https://maps.google.com/maps?ll=47.631764,19.128778&z=16&t=m&hl=hu&gl=HU&mapclient=embed&q=F%C5%91%20%C3%BAt%2024%20Dunakeszi%202120" target="_blank">
         <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z">
@@ -7,6 +8,7 @@
         2120 Dunakeszi, Fő út 24.
     </a>
 
+    <!-- email -->
     <a class="contactbar-item" href="mailto:andrea.cserpes@optometeroptika.hu">
         <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path d="M4 0a2 2 0 0 0-2 2v1.133l-.941.502A2 2 0 0 0 0 5.4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V5.4a2 2 0 0 0-1.059-1.765L14 3.133V2a2 2 0 0 0-2-2H4Zm10 4.267.47.25A1 1 0 0 1 15 5.4v.817l-1 .6v-2.55Zm-1 3.15-3.75 2.25L8 8.917l-1.25.75L3 7.417V2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v5.417Zm-11-.6-1-.6V5.4a1 1 0 0 1 .53-.882L2 4.267v2.55Zm13 .566v5.734l-4.778-2.867L15 7.383Zm-.035 6.88A1 1 0 0 1 14 15H2a1 1 0 0 1-.965-.738L8 10.083l6.965 4.18ZM1 13.116V7.383l4.778 2.867L1 13.117Z">
@@ -14,6 +16,7 @@
         andrea.cserpes@optometeroptika.hu
     </a>
 
+    <!-- phone -->
     <span class="contactbar-item">
         <svg width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z">
@@ -22,5 +25,5 @@
     </span>
 
     <?php $icon_size = "16"; require("socials.php"); ?>
-
+    
 </div>
